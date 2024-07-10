0d0567fd1c03e5249d8cedc9198b5de367fe7cbc
Alessia Parlato <alessiaparlato@hotmail.it>
2024-07-10 15:02:22 +0200

    ...

--- array06/snack-06.js
+++ array06/snack-06.js
@@ -1,2 +1,22 @@
 /*snack 06
-Fate generare un numero random da 0 a 10 al computer, e chiedete all’utente di inserire un suo numero. Se il numero scelto dall’utente è uguale al numero del computer informate l’utente che ha vinto, altrimenti ha perso.*/
+Fate generare un numero random da 0 a 10 al computer,
+e chiedete all’utente di inserire un suo numero.
+Se il numero scelto dall’utente è uguale al numero del computer
+informate l’utente che ha vinto, altrimenti ha perso.*/
+
+//console.log('ARRAY 06');
+
+//Fate generare un numero random da 0 a 10 al computer.
+//const numeroACaso = Math.floor(Math.random() * 11);
+
+//Chiedete all’utente di inserire un suo numero.
+//let numeroUtente = parseInt(prompt("Inserisci un numero da 0 a 10:"));
+
+//Se il numero scelto dall’utente è uguale al numero del computer..
+//if (numeroUtente === numeroACaso) {
+//..informate l’utente che ha vinto,
+// console.log("Complimenti! Hai indovinato il numero, hai vinto!");
+//} else {
+//..altrimenti ha perso.
+// console.log("Mi dispiace, hai perso. Il numero era " + numeroACaso);
+//}
