bafc00eb1527392dad48e80f73654e25d0d58039
Alessia Parlato <alessiaparlato@hotmail.it>
2024-07-10 13:39:45 +0200

    correzione03

--- array03/snack-03.js
+++ array03/snack-03.js
@@ -2,7 +2,7 @@
 Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.*/
 
-console.log('ARRAY 02');
+console.log('ARRAY 03');
 
 let numero = 0;
 
